fc39ce3392c5a9b667e110733d6c5ad73447f656
Kanishk Shrivastava <shrivastavakanishk3@gmail.com>
2025-05-10 15:26:51 +0530

    ch

---
+++ test.js
@@ -0,0 +1,25 @@
+
+
+
+const N = 10;
+
+
+
+
+function gamer() {
+    console.log("gamer")
+}
+
+
+for (let i = 1; i <= N; i++) {
+    gamer();
+}
+
+
+console.log(`gamer() fn was called, ${N} times!`)
+
+/* 
+Works really well, screen recording is also really dope man,
+not sure how I got it so early,
+couldn't have imagined even slightly... but thank u /|\
+*/
