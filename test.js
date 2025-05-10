1f0625677aab1e17f98af53d153a71e357ac5d6e
Kanishk Shrivastava <shrivastavakanishk3@gmail.com>
2025-05-10 15:52:57 +0530

    ch

--- test.js
+++ test.js
@@ -1,20 +1,12 @@
-
-
-
 const N = 10;
-
-
-
 
 function gamer() {
     console.log("gamer")
 }
 
-
 for (let i = 1; i <= N; i++) {
     gamer();
 }
-
 
 console.log(`gamer() fn was called, ${N} times!`)
 
